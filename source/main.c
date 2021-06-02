0aaaefaab45d918477c606d179cab1ded10cbe36
Rishab Dudhia <rishabdudhia@gmail.com>
2021-06-02 14:07:03 -0700

    Headers

--- source/main.c
+++ source/main.c
@@ -1,9 +1,16 @@
-/*	Author: lab
+/*	Author: Rishab Dudhia
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section: 022
+ *	Assignment: Lab #11  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
- *
+ *	Gambling Game: 
+ *	5 LEDs will light one at a time in a row. The player's goal is to press the stop
+ *	button while the middle LED is lit. Before the round begins, the player will
+ *	determine how many points they wish to bet from their "bank" which will be 
+ *	displayed on the LED matrix. They will press buttons to bet points from their bank, 
+ *	and their bet will be taken from their bank and the number of their bet will be 
+ *	displayed on the seven segment display. The player can increase or decrease their bet 
+ *	by the increase and decrease buttons, respectively. 
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
